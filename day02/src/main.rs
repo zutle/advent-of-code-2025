0840a9e3c491f80d389879f6399e40a2f35bcda1
Lars Haukli <lars@kodetracer.com>
2025-12-02 17:19:54 +0100

    Break, don't continue

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -3,14 +3,12 @@ use std::fs;
 const INPUT_FILE: &'static str = "../input/input_day_02.txt";
 
 fn main() {
-    // if let Err(error) = day2_part_one() {
-    //     eprintln!("error: {}", error);
-    // }
-    println!("start");
+    if let Err(error) = day2_part_one() {
+        eprintln!("error: {}", error);
+    }
     if let Err(error) = day2_part_two() {
         eprintln!("error: {}", error);
     }
-    // test();
 }
 
 #[allow(dead_code)]
@@ -24,7 +22,7 @@ fn day2_part_one() -> std::io::Result<()> {
         }
     }
 
-    println!("sum invalid ids: {}", sum_invalid_ids);
+    println!("sum invalid ids part one: {}", sum_invalid_ids);
     Ok(())
 }
 
@@ -55,8 +53,6 @@ fn find_duplicates_part_one(start: &str, end: &str) -> std::io::Result<u64> {
 
 fn day2_part_two() -> std::io::Result<()> {
     let input = fs::read_to_string(INPUT_FILE)?;
-    // let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
-
     let mut sum_invalid_ids: u64 = 0;
     for range in input.trim().split(",") {
         if let Some((start, end)) = range.trim().split_once("-") {
@@ -64,7 +60,7 @@ fn day2_part_two() -> std::io::Result<()> {
         }
     }
 
-    println!("sum invalid ids: {}", sum_invalid_ids);
+    println!("sum invalid ids part two: {}", sum_invalid_ids);
     Ok(())
 }
 
@@ -94,7 +90,6 @@ fn find_duplicates_part_two(start: &str, end: &str) -> std::io::Result<u64> {
             }
         }
         if all_equal {
-            println!("invalid: {}", id);
             sum_invalid_ids += id;
             continue;
         }
@@ -106,9 +101,8 @@ fn find_duplicates_part_two(start: &str, end: &str) -> std::io::Result<u64> {
 
         for split_into in 2..=id_string.len() / 2 {
             if is_invalid(&id_string, split_into) {
-                println!("invalid: {}", id_string);
                 sum_invalid_ids += id;
-                continue;
+                break;
             }
         }
     }
@@ -116,30 +110,15 @@ fn find_duplicates_part_two(start: &str, end: &str) -> std::io::Result<u64> {
 }
 
 fn is_invalid(id: &str, split_into: usize) -> bool {
-    // println!("id: {}", id);
-    // println!("split into: {}", split_into);
     if id.len() % split_into != 0 {
         return false;
     }
     let chunk_size = id.len() / split_into;
     let first_chunk = &id[0..chunk_size];
-    // println!("chunk size: {}", chunk_size);
     for chunk_num in 1..split_into {
-        // println!("comparing {} to {}", first_chunk, &id[chunk_num*chunk_size..(chunk_num+1)*chunk_size]);
         if &id[chunk_num * chunk_size..(chunk_num + 1) * chunk_size] != first_chunk {
             return false;
         }
     }
     true
-}
-
-fn test() -> bool {
-    let id = "828828828";
-    for split_into in 2..=id.len() / 2 {
-        if is_invalid(id, split_into) {
-            println!("invalid: {}", id);
-            return true;
-        }
-    }
-    false
 }
